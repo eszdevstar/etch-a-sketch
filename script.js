301a923344ed081c53cedff16052200717aa7a64
eszdevstar <devstaresz@gmail.com>
2022-11-11 23:11:48 -0300

    set hover

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 for (let i = 1; i <= 16; i++) {
     document.getElementById('container').innerHTML +=  (`<div id="item" class="item-${i}">${i}</div>`);
 }
+
+const items = document.querySelectorAll('#item');
+
+items.forEach(item => {
+    item.addEventListener('mouseover', function () {
+        this.style.backgroundColor = "#"+((1<<24)*Math.random()|0).toString(16);;
+    });
+});
