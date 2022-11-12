80ecc0f399e5d6113db08e69bc4af19ed2a1fd6c
eszdevstar <devstaresz@gmail.com>
2022-11-12 00:45:56 -0300

    Make button and flex logic

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const body = document.querySelector('body');
+const wrapper = document.createElement('div')
+wrapper.className = 'wrapper';
+body.prepend(wrapper);
+const button = document.createElement('button');
+wrapper.append(button);
 
-for (let i = 1; i <= 16; i++) {
+let number;
+while (number == null || number == "" || number < 0 || number > 100 || isNaN(number) ) {
+   number = parseInt(prompt("Please enter your number", "0 to 100"), 10);
+}
+
+for (let i = 1; i <= number; i++) {
     document.getElementById('container').innerHTML +=  (`<div id="item" class="item-${i}">${i}</div>`);
 }
 
